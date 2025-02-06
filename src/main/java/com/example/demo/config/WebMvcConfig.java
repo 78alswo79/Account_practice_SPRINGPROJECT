2c7c848203761d9e932a78c2f2e43735b16cf141
KEPHAS_LEEMINJAE <78alswo79@gmail.com>
2025-02-06 13:19:01 +0900

    특정 Url 인터셉터 등록 및 인터셉터 로직 처리

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -1,18 +1,34 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.demo.handler.MyInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 
+	@Autowired
+	MyInterceptor myInterceptor;
+	
 	// 공부하기 좋은.
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		// 서버 실행 시, 루트 진입 설정해주기.
 		registry.addRedirectViewController("/", "/index.do");
 	}
+
+	@Override
+	// 인터셉터 등록. 공부하기 좋은
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(myInterceptor)
+				.addPathPatterns("/addAccount.do", "/getDateMonth.do", "/getMyAccountList.do");					// 인터셉터를 적용할 URL 매핑
+	}
+	
+	
 	
 	
 }
---
+++ src/main/java/com/example/demo/handler/MyInterceptor.java
@@ -0,0 +1,84 @@
+package com.example.demo.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Component
+public class MyInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// 요청 처리전에 실행할 로직.
+		// 세션의 "user"속성을 제외한 속성을 초기화한다.
+		sessionUserExcludeDel(request);
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// 요청 처리후 실행할 로직
+		setSessionAttribute(request);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+		// 요청 완료후 실행할 로직
+	}
+
+	/**
+	 * <p>"/addAccount.do", "/getDateMonth.do", "/getMyAccountList.do" 3개 요청에 대해<br>
+	 * session의 "user"속성을 제외한 속성을 초기화한다.</p>
+	 * @author kephas_LEEMINJAE
+	 * @param HttpServletRequest
+	 * @return void
+	 * */
+	private void sessionUserExcludeDel(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		if (session != null) {
+			var allSessionNames = session.getAttributeNames();
+			while (allSessionNames.hasMoreElements()) {
+				String attrName = allSessionNames.nextElement();
+				if (!attrName.equalsIgnoreCase("user")) {
+					session.removeAttribute(attrName);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * <p>인터셉트에 등록한 Url 매핑에 필요한 정보 세션에 세팅.</p>
+	 * @author kephas_LEEMINJAE
+	 * @param HttpServletRequest
+	 * @return void
+	 * */
+	private void setSessionAttribute(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		int endIndexNo = request.getRequestURL().lastIndexOf("/");
+		
+		switch (request.getRequestURI()) {
+		
+			case "/getDateMonth.do":
+					session.setAttribute("redirectUrl", request.getRequestURL());
+					session.setAttribute("param", "year=".concat(request.getParameter("year")));
+				break;
+			default:
+					session.setAttribute("redirectUrl", request.getRequestURL());
+					session.setAttribute("param", "year=".concat(request.getParameter("year")).concat("&month=").concat(request.getParameter("month")));
+				break;
+		}
+		
+		System.out.println("세션 저장된 정보>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + session.getAttribute("redirectUrl"));
+		System.out.println("세션 저장된 정보>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + session.getAttribute("param"));
+	}
+	
+}
