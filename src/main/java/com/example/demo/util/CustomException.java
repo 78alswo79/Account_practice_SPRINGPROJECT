a51f08fb173163a5696bf1d78837a4dfc9990dca
DavidLee <78alswo79@gmail.com>
2025-04-21 00:52:11 +0900

    📝 Chore: UID 버전 추가

--- src/main/java/com/example/demo/util/CustomException.java
+++ src/main/java/com/example/demo/util/CustomException.java
@@ -2,6 +2,11 @@ package com.example.demo.util;
 
 public class CustomException extends RuntimeException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8247141172385937167L;
+
 	public CustomException(String message) {
 		super(message);
 	}
