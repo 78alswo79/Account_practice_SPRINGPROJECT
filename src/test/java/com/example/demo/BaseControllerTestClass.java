423c7e6dce80fd9948d30197b8660f08c393ba15
DavidLee <78alswo79@gmail.com>
2025-04-27 00:20:30 +0900

    🚨 Fix: Juit 테스트

---
+++ src/test/java/com/example/demo/BaseControllerTestClass.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.example.demo.service.AccountService;
+
+public class BaseControllerTestClass {
+
+	@Mock
+	private AccountService accountService;
+	
+//	@InjectMocks
+//    private BaseControllerTestClass baseControllerTestClass;
+
+//	public BaseControllerTestClass(AccountService accoutService) {
+//		this.accoutService = accoutService;
+//	}
+	@BeforeEach
+    public void setUp() {
+		MockitoAnnotations.openMocks(this);
+        // Mocking the behavior of accountService
+        when(accountService.getDateYear()).thenReturn(List.of(2025, 2034));
+    }
+	
+	@Test
+	public void getDateYear() {
+		List<Integer> expectList = List.of(2025,2034);
+		List<Integer> showYearList = accountService.getDateYear();
+		
+		assertNotNull(showYearList);			// List객체가 널인지 판단
+		
+		assertFalse(showYearList.isEmpty());	// 인자가 false일 때, 테스트 성공. 인자가 true면 테스트 실패.
+		
+		assertIterableEquals(expectList, showYearList, "둘 다 다릅니다!!");		// List 컬렉션 객체
+//		System.out.println(assertIterableEquals(expectList, showYearList, "둘 다 다릅니다!!"));
+	}
+	
+	
+	
+	
+	
+}
